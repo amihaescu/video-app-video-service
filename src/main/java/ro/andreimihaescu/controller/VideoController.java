49214b4ba05226047e641e0766b09b5389a5959e
Andrei Mihaescu <andrei.mihaescu91@gmail.com>
2016-09-11 18:58:06 +0300

    - changed path to video file.

--- src/main/java/ro/andreimihaescu/controller/VideoController.java
+++ src/main/java/ro/andreimihaescu/controller/VideoController.java
@@ -2,6 +2,7 @@ package ro.andreimihaescu.controller;
 
 import org.apache.catalina.servlet4preview.http.HttpServletRequest;
 import org.apache.log4j.Logger;
+import org.springframework.http.HttpRequest;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,7 +21,7 @@ public class VideoController {
     private final static Logger LOGGER = Logger.getLogger(VideoController.class);
 
     @RequestMapping("/list")
-    public List<MovieResponse> getMovieListByUserRole() {
+    public List<MovieResponse> getMovieListByUserRole(HttpServletRequest request) {
         LOGGER.info("Returning movie list");
         return Arrays.asList(
                 new MovieResponse(1L, "Silicon Valley", "S03E01", "","/api/movies/1"),
@@ -32,7 +33,7 @@ public class VideoController {
     @RequestMapping("/{video}")
     public void getMovie(@PathVariable("video")Long videoNo, HttpServletRequest request, HttpServletResponse response) throws Exception{
         LOGGER.info(String.format("Serving video %d", videoNo));
-        MultipartFileSender.fromPath(Paths.get("/media/andrei/Data2/Downloads/Silicon.Valley.S03E01.HDTV.x264-KILLERS/Silicon.Valley.S03E01.HDTV.x264-KILLERS.mkv"))
+        MultipartFileSender.fromPath(Paths.get("./Silicon.Valley.S03E01.HDTV.x264-KILLERS.mkv"))
                 .with(request)
                 .with(response)
                 .serveResource();
